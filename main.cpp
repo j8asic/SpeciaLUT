504c427a3aebea69f47c6a002db6cd81cf8a5e15
Josip Basic <j8asic@gmail.com>
2021-05-14 12:36:00 +0200

    Initial commit of the lib and test

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "specialut.hpp"
+
+struct Test
+{
+    template<bool i, bool j, int k, bool l>
+    static void run(const char* message)
+    {
+        std::cout << "Specialization: (" << i << "," << j << "," << k << "," << l << ") message: " << message << std::endl;
+    }
+};
+
+int main()
+{
+
+    SpeciaLUT::Chooser<Test, void(const char*), 2, 2, 3, 2> test;
+
+    int o = 0, l = 1, z = 2;
+    test(o,o,z,l)("Hi");
+    test(o,l,o,o)("this");
+    test(l,o,l,l)("is");
+    test(l,l,z,o)("test");
+
+    return 0;
+}
+
+
---
+++ specialut.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <array>
+#include <utility>
+
+namespace SpeciaLUT {
+
+
+    namespace detail
+    {
+
+        template<std::size_t ND>
+        int flat_offset(std::array<std::size_t, ND> ns, std::array<int, ND> is)
+        {
+            int off = 0;
+            for (int i = ND-1; i >= 0; i--)
+                off = is[i] + off * ns[i];
+            return off;
+        }
+
+        template<std::size_t ND>
+        constexpr std::size_t unflatten(std::size_t i, std::size_t target_level, std::array<std::size_t, ND> const ns, std::size_t level = 0, std::size_t product = 1)
+        {
+            if (level == target_level)
+                return ((i / product) % ns[level]);
+            return unflatten<ND>(i, target_level, ns, level + 1, product * ns[level]);
+        }
+
+    }
+
+
+    /// Runtime choosing of specialized template functions
+    template <typename FnStruct, typename FnSignature, std::size_t... NS>
+    class Chooser {
+
+    private:
+
+        static constexpr std::size_t n_dims_ = sizeof...(NS);
+        static constexpr std::size_t n_ptrs_ = (NS * ...);
+
+        using FnLUT = std::array<FnSignature*, n_ptrs_>;
+
+        template<int i, std::size_t... I>
+        static constexpr FnSignature* fn_ptr(const std::index_sequence<I...>)
+        {
+            return &FnStruct::template run< detail::unflatten<n_dims_>(i, I, {NS...})... >;
+        }
+
+        static constexpr FnLUT ptrs_ = []<std::size_t... I>(std::index_sequence<I...>) -> FnLUT
+        {
+            return {(fn_ptr<I>(std::make_index_sequence<n_dims_>{}))...};
+        } (std::make_index_sequence<n_ptrs_>{});
+
+    public:
+
+        Chooser() = default;
+        ~Chooser() = default;
+
+        /// Get the specialized function deduced from given runtime parameters
+        template <typename...Indices>
+        FnSignature const& operator()(Indices... indices) const
+        {
+            static_assert (sizeof...(indices) == sizeof...(NS), "Template called with inappropriate number of arguments.");
+            return *ptrs_[detail::flat_offset<sizeof...(NS)>({NS...}, {indices...})];
+        }
+
+    };
+
+
+}
